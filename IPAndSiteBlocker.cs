07eaed7cb394d321860964f39091c07eaf5053f4
PattHs <87895614+patthsone@users.noreply.github.com>
2025-08-07 19:34:33 +0300

    Add files via upload

---
+++ IPAndSiteBlocker.cs
@@ -0,0 +1,283 @@
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Utils;
+using CounterStrikeSharp.API.Modules.Admin;
+using CounterStrikeSharp.API.Modules.Memory;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using System.Text.Json;
+
+namespace SiteAndIPBlocker;
+
+public class SiteAndIPBlockerConfig : BasePluginConfig
+{
+    [JsonPropertyName("whitelist")]
+    public List<string> Whitelist { get; set; } = new List<string>();
+
+    [JsonPropertyName("block_message")]
+    public string BlockMessage { get; set; } = "{darkred}Blocked: Sending IP addresses or websites is not allowed.";
+    
+    [JsonPropertyName("name_action")]
+    public int NameAction { get; set; } = 0;
+
+    [JsonPropertyName("rename_message")]
+    public string RenameMessage { get; set; } = "{darkred}Your name contains a blocked IP address or website. It will be renamed.";
+
+    [JsonPropertyName("admin_immunity")]
+    public int AdminImmunity { get; set; } = 0;
+
+    [JsonPropertyName("ConfigVersion")]
+    public override int Version { get; set; } = 1;
+}
+
+public class SiteAndIPBlocker : BasePlugin, IPluginConfig<SiteAndIPBlockerConfig>
+{
+    public override string ModuleName => "Site and IP Blocker";
+    public override string ModuleVersion => "0.0.1";
+    public override string ModuleAuthor => "PattHs and Luxecs2";
+    public override string ModuleDescription => "Блокировка сайтов и IP-адресов в чате + имена игроков.";
+
+    public SiteAndIPBlockerConfig Config { get; set; } = null!;
+
+    private static readonly Regex UrlOrIpRegex = new(@"(\b(?:https?|ftp):\/\/[^\s/$.?#].[^\s]*|(?:\d{1,3}\.){3}\d{1,3}\b)", RegexOptions.IgnoreCase);
+
+    public void OnConfigParsed(SiteAndIPBlockerConfig config)
+    {
+        Config = config;
+        UpdateConfig(config);
+    }
+
+    private static readonly string AssemblyName = Assembly.GetExecutingAssembly().GetName().Name ?? "";
+    private static readonly string CfgPath = $"{Server.GameDirectory}/csgo/addons/counterstrikesharp/configs/plugins/{AssemblyName}/{AssemblyName}.json";
+
+    private static readonly List<string> CommonDomains = new List<string>
+    {
+        ".com",
+        ".net",
+        ".org",
+        ".info",
+        ".biz",
+        ".us",
+        ".ru",
+        ".online",
+        ".su",
+        ".co",
+        ".io",
+        ".me",
+        ".tv",
+        ".edu"
+    };
+
+    private void UpdateConfig<T>(T config) where T : BasePluginConfig, new()
+    {
+        var newCfgVersion = new T().Version;
+
+        if (config.Version == newCfgVersion)
+            return;
+
+        config.Version = newCfgVersion;
+
+        var updatedJsonContent = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(CfgPath, updatedJsonContent);
+
+        Console.WriteLine($"Config updated for V{newCfgVersion}.");
+    }
+
+    public override void Load(bool hotReload)
+    {
+        AddCommandListener("say", OnPlayerChatAll);
+        AddCommandListener("say_team", OnPlayerChatTeam);
+    }
+
+    private static readonly Dictionary<string, char> ColorMap = new Dictionary<string, char>
+    {
+        { "{default}", ChatColors.Default },
+        { "{white}", ChatColors.White },
+        { "{darkred}", ChatColors.DarkRed },
+        { "{green}", ChatColors.Green },
+        { "{lightyellow}", ChatColors.LightYellow },
+        { "{lightblue}", ChatColors.LightBlue },
+        { "{olive}", ChatColors.Olive },
+        { "{lime}", ChatColors.Lime },
+        { "{red}", ChatColors.Red },
+        { "{lightpurple}", ChatColors.LightPurple },
+        { "{purple}", ChatColors.Purple },
+        { "{grey}", ChatColors.Grey },
+        { "{yellow}", ChatColors.Yellow },
+        { "{gold}", ChatColors.Gold },
+        { "{silver}", ChatColors.Silver },
+        { "{blue}", ChatColors.Blue },
+        { "{darkblue}", ChatColors.DarkBlue },
+        { "{bluegrey}", ChatColors.BlueGrey },
+        { "{magenta}", ChatColors.Magenta },
+        { "{lightred}", ChatColors.LightRed },
+        { "{orange}", ChatColors.Orange }
+    };
+
+    private string ReplaceColorPlaceholders(string message)
+    {
+        if (!string.IsNullOrEmpty(message) && message[0] != ' ')
+        {
+            message = " " + message;
+        }
+        
+        foreach (var colorPlaceholder in ColorMap)
+        {
+            message = message.Replace(colorPlaceholder.Key, colorPlaceholder.Value.ToString());
+        }
+        return message;
+    }
+
+    private bool ContainsUrlOrIp(string message)
+    {
+        return UrlOrIpRegex.IsMatch(message) || CommonDomains.Any(domain => message.IndexOf(domain, StringComparison.OrdinalIgnoreCase) >= 0);
+    }
+
+    private string CleanName(string name)
+    {
+        name = UrlOrIpRegex.Replace(name, "");
+        
+        foreach (var domain in CommonDomains)
+        {
+            name = Regex.Replace(name, $@"\b\w+{Regex.Escape(domain)}\b", "", RegexOptions.IgnoreCase);
+        }
+
+        return name.Trim();
+    }
+
+    private HookResult OnPlayerChatAll(CCSPlayerController? player, CommandInfo message)
+    {
+        if (player == null || !player.IsValid || player.IsBot || string.IsNullOrEmpty(message.GetArg(1)))
+            return HookResult.Handled;
+
+        if (Config.AdminImmunity == 1 && AdminManager.PlayerHasPermissions(player, "@css/generic"))
+        {
+            return HookResult.Continue;
+        }
+
+        string chatMessage = message.GetArg(1);
+
+        if (ContainsUrlOrIp(chatMessage))
+        {
+            if (!IsWhitelisted(chatMessage))
+            {
+                string blockMessage = ReplaceColorPlaceholders(Config.BlockMessage);
+                
+                player.PrintToChat(blockMessage);
+                return HookResult.Handled;
+            }
+        }
+
+        return HookResult.Continue;
+    }
+
+    private HookResult OnPlayerChatTeam(CCSPlayerController? player, CommandInfo message)
+    {
+        if (player == null || !player.IsValid || player.IsBot || string.IsNullOrEmpty(message.GetArg(1)))
+            return HookResult.Handled;
+
+        if (Config.AdminImmunity == 1 && AdminManager.PlayerHasPermissions(player, "@css/generic"))
+        {
+            return HookResult.Continue;
+        }
+
+        string chatMessage = message.GetArg(1);
+
+        if (ContainsUrlOrIp(chatMessage))
+        {
+            if (!IsWhitelisted(chatMessage))
+            {
+                string blockMessage = ReplaceColorPlaceholders(Config.BlockMessage);
+                
+                player.PrintToChat(blockMessage);
+                return HookResult.Handled;
+            }
+        }
+
+        return HookResult.Continue;
+    }
+
+    [GameEventHandler]
+    public HookResult OnPlayerConnectFull(EventPlayerConnectFull @event, GameEventInfo info)
+    {
+        checkPlayerName(@event.Userid);
+        return HookResult.Continue;
+    }
+
+    private void checkPlayerName(CCSPlayerController? player)
+    {
+        if (player == null || player.IsBot)
+        {
+            return;
+        }
+        string playerName = player.PlayerName;
+        
+        if (ContainsUrlOrIp(playerName))
+        {
+            if (!IsWhitelisted(playerName))
+            {
+                if (Config.NameAction == 0)
+                {
+                    NativeAPI.IssueServerCommand($"kickid {player.UserId}");
+                }
+                else if (Config.NameAction == 1)
+                {
+                    RenamePlayer(player);
+                }
+            }
+        }
+    }
+
+    [GameEventHandler]
+    public HookResult OnPlayerChangeName(EventPlayerChangename @event, GameEventInfo info)
+    {
+        CCSPlayerController player = @event.Userid;
+
+        if (player == null || player.IsBot)
+        {
+            return HookResult.Continue;
+        }
+        
+        string newName = @event.Newname;
+        
+        if (ContainsUrlOrIp(newName))
+        {
+            if (!IsWhitelisted(newName))
+            {
+                if (Config.NameAction == 0)
+                {
+                    NativeAPI.IssueServerCommand($"kickid {player.UserId}");
+                    return HookResult.Handled;
+                }
+                else if (Config.NameAction == 1)
+                {
+                    RenamePlayer(player);
+                }
+            }
+        }
+        
+        return HookResult.Continue;
+    }
+
+    private void RenamePlayer(CCSPlayerController player)
+    {
+        string cleanedName = CleanName(player.PlayerName);
+
+        player.PlayerName = cleanedName;
+
+        Utilities.SetStateChanged(player, "CBasePlayerController", "m_iszPlayerName");
+
+        string renameMessage = ReplaceColorPlaceholders(Config.RenameMessage);
+        player.PrintToChat(renameMessage);
+    }
+
+    private bool IsWhitelisted(string message)
+    {
+        string lowerCaseMessage = message.ToLowerInvariant();
+
+        return Config.Whitelist.Any(whitelistedItem => lowerCaseMessage.Contains(whitelistedItem.ToLowerInvariant()));
+    }
+}
---
+++ README.md
@@ -0,0 +1,43 @@
+# SiteAndIPBlocker
+A plugin that blocks websites and IP addresses (name + chat), with a configurable whitelist for allowed sites and IPs.
+
+## Config
+The configuration file will be automatically generated in `csgo/addons/counterstrikesharp/configs/plugins/IPAndSiteBlocker/IPAndSiteBlocker.json`
+```json
+{
+  "whitelist": [
+    "1.1.1.1"
+  ],
+  "block_message": "{darkred}Blocked: Sending IP addresses or websites is not allowed.",
+  "name_action": 1,
+  "rename_message": "{darkred}Your name contains a blocked IP address or website. It will be renamed.",
+  "admin_immunity": 0,
+  "ConfigVersion": 3
+}
+```
+
+## Available colors
+```
+{default}
+{white}
+{darkred}
+{green}
+{lightyellow}
+{lightblue}
+{olive}
+{lime}
+{red}
+{lightpurple}
+{purple}
+{grey}
+{yellow}
+{gold}
+{silver}
+{blue}
+{darkblue}
+{bluegrey}
+{magenta}
+{lightred}
+{orange}
+```
+
