e37f325a2862b365e36c4ba91d39cef249e2123a
PattHs <87895614+patthsone@users.noreply.github.com>
2025-11-12 05:35:57 +0300

    Remove // comments from ExampleUsage.cs

--- ExampleUsage.cs
+++ ExampleUsage.cs
@@ -4,11 +4,9 @@ class Program
 {
     static void Main()
     {
-        // Настройка whitelist
         BlockChecker.Whitelist.Add("google.com");
         BlockChecker.Whitelist.Add("192.168.1.1");
 
-        // Примеры сообщений
         string[] messages = {
             "Check out this site: http://example.com",
             "My IP is 192.168.1.100",
@@ -22,7 +20,6 @@ class Program
             Console.WriteLine($"Message: '{msg}' - Blocked: {blocked}");
         }
 
-        // Пример очистки имени
         string playerName = "Player with http://bad.com in name";
         string cleanedName = BlockChecker.CleanName(playerName);
         Console.WriteLine($"Original name: '{playerName}' - Cleaned: '{cleanedName}'");
